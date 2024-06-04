0cf66bd896fb786aea6bd89670c3b54cca120bac
SimonAwiti <awitisimon23@gmail.com>
2024-06-04 22:28:50 +0300

    filtering associative arrays

--- refresher.php
+++ refresher.php
@@ -10,10 +10,17 @@ $nbaPlayers = [
         'name' => 'Michael Jordan',
         'Team' => 'Bulls',
         'Age' => '50'
+    ],
+    [
+        'name' => 'Scot Pippen',
+        'Team' => 'Bulls',
+        'Age' => '52'
     ]
     ];
-foreach($nbaPlayers as $nbaPlayer){
-    echo $nbaPlayer['name'];
-    echo $nbaPlayer['Team']."<br>\n";
-}
+
+    foreach($nbaPlayers as $nbaPlayer){
+        if ($nbaPlayer['Team'] === 'Bulls'){
+            echo $nbaPlayer['name'];
+        }
+    }
 ?>
